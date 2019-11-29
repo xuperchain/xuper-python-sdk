8224b521b2394aa0d696f219e11e14f441058dee
fxsjy <ccnusjy@gmail.com>
2019-11-29 10:13:23 +0800

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ A pure python sdk for XuperChain
 """
 
 setup(name='xuper',
-      version='0.04',
+      version='0.05',
       description='Pure Python SDK for XuperChain',
       long_description=LONGDOC,
       author='Sun, Junyi',
